d3dcbc262aae538559630edac295f6c770fa9423
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-11 16:36:25 -0700

    Prune legacy tests

--- performance/ITERATOR_VERSIONS.js
+++ performance/ITERATOR_VERSIONS.js
@@ -4,11 +4,7 @@ module.exports = [
     Iterator: require('..'),
   },
   {
-    version: '1.3.0',
-    Iterator: require('fs-iterator-1.3.0'),
-  },
-  {
-    version: '1.1.0',
-    Iterator: require('fs-iterator-1.1.0'),
+    version: '2.1.0',
+    Iterator: require('fs-iterator-2.1.0'),
   },
 ];
