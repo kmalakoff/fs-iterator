d2d0965f302d2688f65510b3c94fb0f3552bde29
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-12 18:31:45 -0700

    Add readdrip performance tests

--- performance/benchmark/index.js
+++ performance/benchmark/index.js
@@ -1,12 +1,18 @@
 const path = require('path');
 
 const tests = require('./tests');
+const readdirp = require('./readdirp');
 
-const WALK_VERSIONS = require('../ITERATOR_VERSIONS');
+const ITERATOR_VERSIONS = require('../ITERATOR_VERSIONS');
+const READDIRP_VERSIONS = require('../READDIRP_VERSIONS');
 const DATA_DIR = path.resolve(path.join(__dirname, '..', 'node_modules'));
 
 (async () => {
-  for (const options of WALK_VERSIONS) {
-    await tests(options, DATA_DIR);
+  // for (const options of ITERATOR_VERSIONS) {
+  //   await tests(options, DATA_DIR);
+  // }
+
+  for (const options of READDIRP_VERSIONS) {
+    await readdirp(options, DATA_DIR);
   }
 })();
---
+++ performance/benchmark/readdirp.js
@@ -0,0 +1,56 @@
+var Benchmark = require('benchmark');
+
+const CONCURRENCIES = [1, 100, 1600, Infinity];
+
+const TESTS = [];
+TESTS.push({ name: `default` });
+for (const concurrency of CONCURRENCIES) {
+  TESTS.push({ name: `${concurrency}`, options: { highWaterMark: concurrency } });
+}
+
+module.exports = async function run({ ReaddirpStream, version }, dir) {
+  console.log('****************\n');
+  console.log(`Running: ${version}`);
+  console.log('----------------');
+
+  return new Promise(function (resolve, reject) {
+    const suite = new Benchmark.Suite('Iterator ' + dir);
+
+    for (const test of TESTS) {
+      suite.add(
+        test.name,
+        async function (deferred) {
+          const stream = new ReaddirpStream(dir, test.options);
+          stream.on('data', function (entry) {});
+          stream.on('error', function (err) {
+            deferred.reject(err);
+          });
+          stream.on('end', function () {
+            deferred.resolve();
+          });
+        },
+        { defer: true }
+      );
+    }
+
+    suite.on('start', function () {
+      console.log('Comparing ' + this.name);
+    });
+    suite.on('cycle', function (event) {
+      console.log(String(event.target));
+    });
+    suite.on('error', function () {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject();
+    });
+    suite.on('complete', function () {
+      var fastest = this.filter('fastest')[0];
+      console.log('----------------\n');
+      console.log('Fastest is ' + fastest.name + ' x ' + fastest.hz.toFixed(2) + ' ops/sec');
+      console.log('****************\n');
+      resolve();
+    });
+    global.gc();
+    suite.run({ async: true, maxTime: 1000 });
+  });
+};
