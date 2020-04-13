d141d7278c8da599de32d141271674619dd940b1
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-12 18:31:45 -0700

    Add readdrip performance tests

---
+++ performance/READDIRP_VERSIONS.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    version: 'iterator',
+    ReaddirpStream: require('../test/spec/readdirp').ReaddirpStream,
+  },
+  // {
+  //   version: '2.2.0',
+  //   Iterator: require('fs-iterator-2.2.0'),
+  // },
+];
