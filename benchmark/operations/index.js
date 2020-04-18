fc35e7ede5a44572293868c506c13bc2f36d76ce
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-18 10:10:57 -0700

    Remove readdirp tests

--- benchmark/operations/index.js
+++ benchmark/operations/index.js
@@ -1,19 +1,13 @@
 const path = require('path');
 
 const tests = require('./tests');
-const readdirp = require('./readdirp');
 
-const ITERATOR_VERSIONS = require('../ITERATOR_VERSIONS');
+const VERSIONS = require('../VERSIONS');
 const TESTS_OPTIONS = require('../TESTS_OPTIONS');
-const READDIRP_VERSIONS = require('../READDIRP_VERSIONS');
 const DATA_DIR = path.resolve(path.join(__dirname, '..', 'node_modules'));
 
 (async () => {
-  for (const options of ITERATOR_VERSIONS) {
+  for (const options of VERSIONS) {
     await tests(Object.assign({}, options, { testOptions: TESTS_OPTIONS }), DATA_DIR);
   }
-
-  // for (const options of READDIRP_VERSIONS) {
-  //   await readdirp(Object.assign({}, options, { testOptions: TESTS_OPTIONS }), DATA_DIR);
-  // }
 })();
