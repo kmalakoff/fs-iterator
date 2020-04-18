15ba5a3c6c23a9b7011a8eaaec46639c317fe209
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-18 10:29:19 -0700

    Remove readdirp tests

--- compatibility/spec/platform-specific.test.js
+++ compatibility/spec/platform-specific.test.js
@@ -4,5 +4,4 @@ const HAS_ASYNC_AWAIT = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
 describe('platform specific', function () {
   if (HAS_ASYNC_ITERATOR) require('./asyncIterator');
   if (HAS_ASYNC_AWAIT) require('./asyncAwait');
-  if (HAS_ASYNC_AWAIT) require('./readdirp/test');
 });
