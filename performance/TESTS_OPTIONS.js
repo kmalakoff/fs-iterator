7f543c59dab06de5de3810fe1bd74fd2ee989e3b
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-21 12:41:55 -0700

    Compare to latest

--- performance/TESTS_OPTIONS.js
+++ performance/TESTS_OPTIONS.js
@@ -2,8 +2,8 @@
 // const CONCURRENCIES = [100];
 // const CONCURRENCIES = [2];
 // const CONCURRENCIES = [1];
-// const CONCURRENCIES = [1, 1000, Infinity];
-const CONCURRENCIES = [1, 100, 1000, Infinity];
+const CONCURRENCIES = [1, 1000, Infinity];
+// const CONCURRENCIES = [1, 100, 1000, Infinity];
 const TESTS_OPTIONS = [];
 
 // TESTS_OPTIONS.push({ name: `default` });
