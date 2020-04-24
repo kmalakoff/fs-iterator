30ed4106d4f6edd1c80711d814a306a824554f53
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-23 19:06:24 -0700

    Update alwaysStat tests to test for types

--- test/spec/alwaysStat.test.js
+++ test/spec/alwaysStat.test.js
@@ -5,6 +5,7 @@ var assert = chai.assert;
 var generate = require('fs-generate');
 var rimraf = require('rimraf');
 var path = require('path');
+var fs = require('fs');
 
 var Iterator = require('../..');
 var statsSpys = require('../utils').statsSpys;
@@ -32,47 +33,151 @@ describe('alwaysStat', function () {
     });
   });
 
-  it('depth 0', function (done) {
-    var spys = statsSpys();
+  describe('default', function () {
+    it('depth 0', function (done) {
+      var spys = statsSpys();
 
-    var iterator = new Iterator(DIR, {
-      depth: 0,
-      filter: function (entry) {
-        spys(entry.stats, entry.path);
-      },
-      lstat: true,
+      var iterator = new Iterator(DIR, {
+        depth: 0,
+        filter: function (entry) {
+          assert.ok(fs.Dirent ? entry.stats instanceof fs.Dirent : entry.stats instanceof fs.Stats);
+          spys(entry.stats, entry.path);
+        },
+        lstat: true,
+      });
+      iterator.forEach(
+        function () {},
+        function (err) {
+          assert.ok(!err);
+          assert.equal(spys.dir.callCount, 3);
+          assert.equal(spys.file.callCount, 2);
+          assert.equal(spys.link.callCount, 1);
+          done();
+        }
+      );
     });
-    iterator.forEach(
-      function () {},
-      function (err) {
-        assert.ok(!err);
-        assert.equal(spys.dir.callCount, 3);
-        assert.equal(spys.file.callCount, 2);
-        assert.equal(spys.link.callCount, 1);
-        done();
-      }
-    );
+
+    it('depth Infinity', function (done) {
+      var spys = statsSpys();
+
+      var iterator = new Iterator(DIR, {
+        depth: Infinity,
+        filter: function (entry) {
+          assert.ok(fs.Dirent ? entry.stats instanceof fs.Dirent : entry.stats instanceof fs.Stats);
+          spys(entry.stats, entry.path);
+        },
+        lstat: true,
+      });
+      iterator.forEach(
+        function () {},
+        function (err) {
+          assert.ok(!err);
+          assert.equal(spys.dir.callCount, 5);
+          assert.equal(spys.file.callCount, 5);
+          assert.equal(spys.link.callCount, 2);
+          done();
+        }
+      );
+    });
   });
 
-  it('depth Infinity', function (done) {
-    var spys = statsSpys();
+  describe('alwaysStat false', function () {
+    it('depth 0', function (done) {
+      var spys = statsSpys();
 
-    var iterator = new Iterator(DIR, {
-      depth: Infinity,
-      filter: function (entry) {
-        spys(entry.stats, entry.path);
-      },
-      lstat: true,
+      var iterator = new Iterator(DIR, {
+        depth: 0,
+        filter: function (entry) {
+          assert.ok(fs.Dirent ? entry.stats instanceof fs.Dirent : entry.stats instanceof fs.Stats);
+          spys(entry.stats, entry.path);
+        },
+        lstat: true,
+        alwaysStat: false,
+      });
+      iterator.forEach(
+        function () {},
+        function (err) {
+          assert.ok(!err);
+          assert.equal(spys.dir.callCount, 3);
+          assert.equal(spys.file.callCount, 2);
+          assert.equal(spys.link.callCount, 1);
+          done();
+        }
+      );
     });
-    iterator.forEach(
-      function () {},
-      function (err) {
-        assert.ok(!err);
-        assert.equal(spys.dir.callCount, 5);
-        assert.equal(spys.file.callCount, 5);
-        assert.equal(spys.link.callCount, 2);
-        done();
-      }
-    );
+
+    it('depth Infinity', function (done) {
+      var spys = statsSpys();
+
+      var iterator = new Iterator(DIR, {
+        depth: Infinity,
+        filter: function (entry) {
+          assert.ok(fs.Dirent ? entry.stats instanceof fs.Dirent : entry.stats instanceof fs.Stats);
+          spys(entry.stats, entry.path);
+        },
+        lstat: true,
+        alwaysStat: false,
+      });
+      iterator.forEach(
+        function () {},
+        function (err) {
+          assert.ok(!err);
+          assert.equal(spys.dir.callCount, 5);
+          assert.equal(spys.file.callCount, 5);
+          assert.equal(spys.link.callCount, 2);
+          done();
+        }
+      );
+    });
+  });
+
+  describe('alwaysStat true', function () {
+    it('depth 0', function (done) {
+      var spys = statsSpys();
+
+      var iterator = new Iterator(DIR, {
+        depth: 0,
+        filter: function (entry) {
+          assert.ok(entry.stats instanceof fs.Stats);
+          spys(entry.stats, entry.path);
+        },
+        lstat: true,
+        alwaysStat: true,
+      });
+      iterator.forEach(
+        function () {},
+        function (err) {
+          assert.ok(!err);
+          assert.equal(spys.dir.callCount, 3);
+          assert.equal(spys.file.callCount, 2);
+          assert.equal(spys.link.callCount, 1);
+          done();
+        }
+      );
+    });
+
+    it('depth Infinity', function (done) {
+      var spys = statsSpys();
+
+      var iterator = new Iterator(DIR, {
+        depth: Infinity,
+        filter: function (entry) {
+          assert.ok(entry.stats instanceof fs.Stats);
+          spys(entry.stats, entry.path);
+        },
+        lstat: true,
+        alwaysStat: true,
+      });
+      iterator.forEach(
+        function () {},
+        function (err) {
+          assert.ok(!err);
+          assert.equal(spys.dir.callCount, 5);
+          assert.equal(spys.file.callCount, 5);
+          assert.equal(spys.link.callCount, 2);
+          done();
+        }
+      );
+    });
   });
 });
