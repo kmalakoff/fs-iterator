de181d008a53a01aa4f3df113d08ee3ecffd872f
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-10 11:48:37 -0700

    Upgrade modules

--- test/spec/platform-specific.test.js
+++ test/spec/platform-specific.test.js
@@ -1,7 +1,5 @@
 const HAS_ASYNC_ITERATOR = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
 const HAS_ASYNC_AWAIT = typeof Symbol !== 'undefined' && Symbol.asyncIterator;
 
-describe('platform specific', function () {
-  if (HAS_ASYNC_ITERATOR) require('./asyncIterator');
-  if (HAS_ASYNC_AWAIT) require('./asyncAwait');
-});
+!HAS_ASYNC_ITERATOR || require('./asyncIterator');
+!HAS_ASYNC_AWAIT || require('./asyncAwait');
