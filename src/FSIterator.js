01e35411c2fa4c939c2a7f61859938f213805302
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-17 12:37:04 -0800

    Upgrade to ts-dev-stack

--- src/FSIterator.js
+++ src/FSIterator.js
@@ -1,12 +1,12 @@
-var inherits = require('inherits');
-var StackBaseIterator = require('stack-base-iterator');
-var fs = require('fs');
-var path = require('path');
+const inherits = require('inherits');
+const StackBaseIterator = require('stack-base-iterator');
+const fs = require('fs');
+const path = require('path');
 
-var PathStack = require('./PathStack');
-var fifoRemove = require('./fifoRemove');
-var fsCompat = require('./fs-compat');
-var lifoFromArray = require('./lifoFromArray');
+const PathStack = require('./PathStack');
+const fifoRemove = require('./fifoRemove');
+const fsCompat = require('./fs-compat');
+const lifoFromArray = require('./lifoFromArray');
 
 function FSIterator(root, options) {
   if (!(this instanceof FSIterator)) return new FSIterator(root, options);
@@ -24,18 +24,16 @@ function FSIterator(root, options) {
 
   this.root = path.resolve(root);
   this.stack = new PathStack();
-
-  var self = this;
-  var cancelled = false;
+  let cancelled = false;
   function setup() {
     cancelled = true;
   }
   this.processing.push(setup);
-  fsCompat.readdir(this.root, this.options.readdir, function (err, files) {
-    fifoRemove(self.processing, setup);
-    if (self.done || cancelled) return;
-    if (err) return self.end(err);
-    if (files.length) self.push({ path: null, depth: 0, files: lifoFromArray(files) });
+  fsCompat.readdir(this.root, this.options.readdir, (err, files) => {
+    fifoRemove(this.processing, setup);
+    if (this.done || cancelled) return;
+    if (err) return this.end(err);
+    if (files.length) this.push({ path: null, depth: 0, files: lifoFromArray(files) });
   });
 }
 
