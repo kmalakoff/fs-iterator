e8cb016058f1782af3fdfc2fd9fce25647a35b8b
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-02 06:09:05 -0800

    Upgrade modules

--- dist/cjs/depthFirst/filter.js
+++ dist/cjs/depthFirst/filter.js
@@ -22,4 +22,4 @@ function filter(iterator, entry, callback) {
         callback(null, true);
     });
 }
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
