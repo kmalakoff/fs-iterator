40fcfa8bb31a6b9357685882ef2274d0dad6e150
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 09:04:49 -0700

    Add generic type support to FSIterator and update depthFirst traversal

--- src/depthFirst/index.ts
+++ src/depthFirst/index.ts
@@ -10,23 +10,23 @@ function isDirectory(entry) {
   return entry.stats.isDirectory();
 }
 
-export default function path(item, iterator: Iterator, callback) {
+export default function path<T>(item, iterator: Iterator<T>, callback) {
   const depth = item.depth;
   const entry = createEntry(iterator, item);
   item = null; // release reference
 
   stat(iterator, entry, function statCallback(err) {
-    if (err || iterator.done) return callback(err);
+    if (err || iterator.isDone()) return callback(err);
 
     filter(iterator, entry, function filterCallback(err, keep) {
-      if (err || !keep || iterator.done) return callback(err);
+      if (err || !keep || iterator.isDone()) return callback(err);
 
       // not a directory or is a directory, but next level is too deep
       if (!isDirectory(entry) || depth + 1 > iterator.depth) return callback(null, entry);
 
       // get files in this directory
       fsCompat.readdir(entry.fullPath, iterator.readdirOptions, function readdirCallback(err, files) {
-        if (err || iterator.done) return callback(err);
+        if (err || iterator.isDone()) return callback(err);
         if (files.length) iterator.push({ path: entry.path, depth: depth + 1, files: lifoFromArray(files) });
         return callback(null, entry);
       });
