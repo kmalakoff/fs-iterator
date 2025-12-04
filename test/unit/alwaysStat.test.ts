081504a8955aaa84d2d8cc86badc4afe163fcc57
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-04 14:42:57 -0800

    Replace rimraf2 with fs-remove-compat in tests

--- test/unit/alwaysStat.test.ts
+++ test/unit/alwaysStat.test.ts
@@ -2,9 +2,9 @@ import assert from 'assert';
 import fs from 'fs';
 import generate from 'fs-generate';
 import Iterator, { type Entry } from 'fs-iterator';
+import { safeRm } from 'fs-remove-compat';
 import statsSpys from 'fs-stats-spys';
 import path from 'path';
-import rimraf2 from 'rimraf2';
 import url from 'url';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
@@ -23,7 +23,7 @@ const STRUCTURE = {
 
 describe('alwaysStat', () => {
   beforeEach((done) => {
-    rimraf2(TEST_DIR, { disableGlob: true }, () => {
+    safeRm(TEST_DIR, () => {
       generate(TEST_DIR, STRUCTURE, (err) => {
         done(err);
       });
--- test/unit/asyncIterator.test.ts
+++ test/unit/asyncIterator.test.ts
@@ -1,10 +1,10 @@
 import assert from 'assert';
 import generate from 'fs-generate';
 import Iterator, { type Entry } from 'fs-iterator';
+import { safeRm } from 'fs-remove-compat';
 import statsSpys from 'fs-stats-spys';
 import path from 'path';
 import Pinkie from 'pinkie-promise';
-import rimraf2 from 'rimraf2';
 import url from 'url';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
@@ -36,7 +36,7 @@ describe('asyncIterator', () => {
   })();
 
   beforeEach((done) => {
-    rimraf2(TEST_DIR, { disableGlob: true }, () => {
+    safeRm(TEST_DIR, () => {
       generate(TEST_DIR, STRUCTURE, (err) => {
         done(err);
       });
