4c6af84f37cfd2990039d97bc0957146294487e9
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 16:47:19 -0800

    Migrate to void

--- src/depthFirst/filter.ts
+++ src/depthFirst/filter.ts
@@ -3,13 +3,13 @@ import compat from 'async-compat';
 import type Iterator from '../FSIterator.ts';
 import type { Entry, IteratorOptions } from '../types.ts';
 
-export type Callback = (error?: Error, keep?: boolean) => undefined;
+export type Callback = (error?: Error, keep?: boolean) => void;
 
 interface IteratorWithOptions {
   options: IteratorOptions;
 }
 
-export default function filter(iterator: Iterator, entry: Entry, callback: Callback): undefined {
+export default function filter(iterator: Iterator, entry: Entry, callback: Callback): void {
   const options = (iterator as unknown as IteratorWithOptions).options;
   if (!options.filter) return callback(null, true);
 
