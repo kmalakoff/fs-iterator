1f189999b10a1dd023b4e26f9ffcef48e33d1e0c
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 21:59:17 -0700

    Fix depth-first traversal and update test configuration

--- performance/ITERATOR_VERSIONS.js
+++ performance/ITERATOR_VERSIONS.js
@@ -3,4 +3,12 @@ module.exports = [
     version: 'latest',
     Iterator: require('..'),
   },
+  {
+    version: '1.2.0',
+    Iterator: require('fs-iterator-1.2.0'),
+  },
+  {
+    version: '1.1.0',
+    Iterator: require('fs-iterator-1.1.0'),
+  },
 ];
